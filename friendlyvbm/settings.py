926d0b7c2bc4c1abc82f8f603cbfb823f8af0e9d
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2023-07-12 11:07:07 -0400

    Create vbm index page

--- friendlyvbm/settings.py
+++ friendlyvbm/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # My apps
+    'vbm_app',
+
+    # Default django apps.
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
